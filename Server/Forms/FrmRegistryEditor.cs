5096236b561d43c4ab70a561ca8abf2905dd0735
yankejustin <yankejustin@hotmail.com>
2015-05-27 11:32:01 -0400

    Fixes and more-accurate logic
    Fixed a few issues and added a bit more logic.

--- Server/Forms/FrmRegistryEditor.cs
+++ Server/Forms/FrmRegistryEditor.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using xServer.Core;
 using Microsoft.Win32;
 
@@ -47,12 +49,38 @@ namespace xServer.Forms
                                 // The lastSubKeyName is empty, so it must be a root node.
                                 if (string.IsNullOrEmpty(lastSubKeyName))
                                 {
-                                    tvRegistryDirectory.Nodes.Add(ParsedKeyNameNodes(SubKey.Name));
+                                    TreeNode NewNode = ParsedKeyNameNodes(SubKey.Name);
+
+                                    if (NewNode != null)
+                                    {
+                                        if (tvRegistryDirectory.Nodes.ContainsKey(NewNode.Text))
+                                        {
+                                            tvRegistryDirectory.Nodes[tvRegistryDirectory.Nodes.IndexOfKey(NewNode.Text)].Nodes.Add(NewNode);
+                                        }
+                                        else
+                                        {
+                                            tvRegistryDirectory.Nodes.Add(NewNode.Text, NewNode.Text);
+                                        }
+                                    }
                                 }
                                 // It is a sub-key.
                                 else
                                 {
-                                    tvRegistryDirectory.Nodes.Add(ParsedKeyNameNodes(key.Name));
+                                    TreeNode NewNode = ParsedKeyNameNodes(key.Name);
+
+                                    if (NewNode != null)
+                                    {
+                                        tvRegistryDirectory.Nodes.Add(NewNode);
+
+                                        if (tvRegistryDirectory.Nodes.ContainsKey(NewNode.Text))
+                                        {
+                                            tvRegistryDirectory.Nodes[tvRegistryDirectory.Nodes.IndexOfKey(NewNode.Text)].Nodes.Add(NewNode);
+                                        }
+                                        else
+                                        {
+                                            tvRegistryDirectory.Nodes.Add(NewNode.Text, NewNode.Text);
+                                        }
+                                    }
                                 }
 
                                 // Load more sub-key directories, but with decrementing levels of depth.
@@ -74,26 +102,90 @@ namespace xServer.Forms
         /// <returns>A collection of the parsed registry key nodes.</returns>
         private TreeNode ParsedKeyNameNodes(string keyName)
         {
-            if (!string.IsNullOrEmpty(keyName) && keyName.Contains("/"))
+            if (!string.IsNullOrEmpty(keyName))
             {
-                string[] KeyPaths = keyName.Split('/');
-
-                TreeNode ParsedTreeNodes = new TreeNode();
-
-                for (int i = 0; i < KeyPaths.Length; i++)
+                // Contains "\\". It must be a sub-node.
+                if (keyName.Contains("\\"))
                 {
-                    if (!string.IsNullOrEmpty(KeyPaths[i]))
-                    {
-                        ParsedTreeNodes.Nodes[i].Nodes.Add(KeyPaths[i]);
-                    }
-                }
+                    string[] KeyPaths = keyName.Split('\\');
 
-                return ParsedTreeNodes;
+                    // Not a valid TreeNode if the root node has no name!
+                    if (string.IsNullOrEmpty(KeyPaths[0]))
+                    {
+                        return null;
+                    }
+
+                    TreeNode ParsedTreeNodes = new TreeNode(KeyPaths[0]);
+
+                    // The loop below assumes that the length of KeyPaths[] is atleast 2.
+                    // Since the array is known to contain an item [>> keyName.Contains("\\") is true <<],
+                    // we know the length is atleast one. After splitting the string based on that element,
+                    // the results are atleast 2 (even though one or both elements can be equal to string.Empty.
+                    for (int i = 1; i < KeyPaths.Length; i++)
+                    {
+                        ParsedTreeNodes.Nodes.Add(new TreeNode(KeyPaths[i]));
+                    }
+
+                    return ParsedTreeNodes;
+                }
+                // Does not contain "\\". It must be a root node.
+                else
+                {
+                    return new TreeNode(keyName);
+                }
             }
             else
             {
-                return new TreeNode(keyName);
+                // Not a valid TreeNode if the root node has a keyName equal to string.Empty.
+                return null;
             }
+        }
+
+        private void PopulateLstRegistryKeys(RegistryKey[] keys)
+        {
+            for (int i = 0; i < lstRegistryKeys.Items.Count; i++)
+            {
+                RegistryKey key = lstRegistryKeys.Items[i].Tag as RegistryKey;
+                if (key != null)
+                {
+                    key.Close();
+                }
+            }
+
+            lstRegistryKeys.Clear();
+
+            if (keys != null)
+            {
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    // To-Do: Use a custom ListViewItem for a better style.
+                    lstRegistryKeys.Items.Add(new ListViewItem() { Text = keys[i].Name, Tag = keys[i] });
+                }
+            }
+        }
+
+        private void tvRegistryDirectory_Click(object sender, EventArgs e)
+        {
+            if ((tvRegistryDirectory.SelectedNode != null) && (tvRegistryDirectory.SelectedNode.Tag != null))
+            {
+                List<RegistryKey> KeysFromNode = new List<RegistryKey>();
+                foreach (object obj in (object[])tvRegistryDirectory.SelectedNode.Tag)
+                {
+                    RegistryKey key = obj as RegistryKey;
+                    if (key != null)
+                    {
+                        KeysFromNode.Add(key);
+                    }
+                }
+
+                PopulateLstRegistryKeys(KeysFromNode.ToArray());
+            }
+        }
+
+        private void FrmRegistryEditor_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_connectClient.Value.FrmRe != null)
+                _connectClient.Value.FrmRe = null;
         }
     }
 }
