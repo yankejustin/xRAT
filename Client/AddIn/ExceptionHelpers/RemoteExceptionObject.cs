f316dd95afe7b81300e02dd6e29cf5963382fc75
yankejustin <yankejustin@hotmail.com>
2015-07-17 13:37:11 -0400

    Added a way to handle Exceptions in the AddIns
    Abstractions are next in preparation for AddIn capability. This is to
    manage issues that come from Addins.
    -----------------------------------------
    I still need an interface on the server to make interacting with AddIns
    more simple. :)

---
+++ Client/AddIn/ExceptionHelpers/RemoteExceptionObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn.Hosting;
+
+namespace xClient.AddIn.ExceptionHelpers
+{
+    /// <summary>
+    /// Provides an object to use that will help retrieve unhandled Exceptions across other Application Domains.
+    /// </summary>
+    public class RemoteExceptionObject : MarshalByRefObject
+    {
+        private AddInToken _token;
+
+        public void Initialize(AddInToken token)
+        {
+            _token = token;
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // This token gave us problems, so mark it as unstable.
+            _token.MarkTokenUnstable();
+        }
+
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+    }
+}
---
+++ Client/AddIn/ExceptionHelpers/UnhandledExceptionHelper.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn.Hosting;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace xClient.AddIn.ExceptionHelpers
+{
+    /// <summary>
+    /// Provides a set of actions to manage unhandled Exceptions in AddIns and set of actions that
+    /// manage unstable AddIns.
+    /// </summary>
+    public static class UnhandledExceptionHelper
+    {
+        private const string unstableTokenFile = "unstableTokens.tokens";
+
+        /// <summary>
+        /// Wires the calling AddIn to a Remote Exception Object so unhandled Exceptions will be handled.
+        /// </summary>
+        /// <param name="addIn">The AddIn to handle unhandled Exceptions from.</param>
+        public static void HandleUnhandledExceptions(this AddInToken addIn)
+        {
+            try
+            {
+                AddInController controller = AddInController.GetAddInController(addIn);
+                AddInToken token = controller.Token;
+                AppDomain domain = controller.AppDomain;
+                RemoteExceptionObject helper =
+                    (RemoteExceptionObject)domain.CreateInstanceFromAndUnwrap(typeof(RemoteExceptionObject).Assembly.Location, typeof(RemoteExceptionObject).FullName);
+
+                helper.Initialize(token);
+
+            }
+            catch
+            { }
+        }
+
+        /// <summary>
+        /// Retrieves a List of unstable tokens.
+        /// </summary>
+        /// <returns></returns>
+        public static List<AddInToken> GetUnstableTokens()
+        {
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                if (File.Exists(unstableTokenFile))
+                {
+                    using (FileStream fs = File.OpenRead(unstableTokenFile))
+                    {
+                        return (List<AddInToken>)formatter.Deserialize(fs);
+                    }
+                }
+                else
+                {
+                    // No unstable tokens...
+                    return new List<AddInToken>();
+                }
+            }
+            catch
+            {
+                // Unable to get the List of unstable tokens. Return an empty List.
+                return new List<AddInToken>();
+            }
+        }
+
+        /// <summary>
+        /// Marks the calling token as unstable.
+        /// </summary>
+        /// <param name="token">The token to mark as unstable.</param>
+        public static void MarkTokenUnstable(this AddInToken token)
+        {
+            List<AddInToken> unstableTokens = GetUnstableTokens();
+            unstableTokens.Add(token);
+
+            // TO-DO: Send a message to the server that states that the token is unstable.
+
+            unstableTokens.WriteUnstableTokens();
+        }
+        
+        /// <summary>
+        /// Writes all of the tokens in the calling List of tokens to a file that holds each unstable token.
+        /// </summary>
+        /// <param name="tokens">The List of unstable tokens to write to the file that holds each unstable token.</param>
+        public static void WriteUnstableTokens(this List<AddInToken> tokens)
+        {
+            try
+            {
+                BinaryFormatter f = new BinaryFormatter();
+
+                using (FileStream fs = File.OpenWrite(unstableTokenFile))
+                {
+                    f.Serialize(File.OpenWrite(unstableTokenFile), tokens);
+                }
+            }
+            catch
+            { }
+        }
+    }
+}
